8f184f2fa0611e77390e4cc09a06cc4683e324e8
y.ono0505 <onoyamayudai067@gmail.com>
2020-11-01 17:28:33 +0900

    課題提出テスト
    1-3の作成

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,25 @@
+console.log("問1");
+
+console.log(a != 10);
+console.log(b >= 10 && b<20 && b % 2 == 0);
+
+console.log("問2");
+
+let x = 10;
+
+if(x > 10 && x < 20) {
+    console.log("成功です");
+}else {
+    console.log("失敗です");
+    
+}
+
+console.log("問3");
+
+let y = 0;
+
+if(y % 2 === 0) {
+    console.log("偶数です");
+}else if(y % 2 != 0) {
+    console.log("奇数です");
+}
